b35b8592d8d8b4b9024ec5db4c4b55e742d4fbe4
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-07-31 13:27:45 +0300

    IEntityRepository created. Existing Dal interfaces edited according to Generic IEntityRepository.

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -7,12 +7,7 @@ using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface ICarDal
+    public interface ICarDal : IEntityRepository<Car>
     {
-        Car GetById(int id);
-        List<Car> GetAll();
-        void Add(Car car);
-        void Update(Car car);
-        void Delete(Car car);
     }
 }
