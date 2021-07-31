7dcece14afd817a3028270b88b662e2e5d418686
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-07-31 09:39:53 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarService carService = new ICarManager(new InMemoryCarDal());
+            Console.WriteLine("Get All:");
+            GetAllCars(carService);
+
+            Console.WriteLine("\nGet by Id:");
+            Console.WriteLine(carService.GetById(1).Description);
+
+            Console.WriteLine("\n---Add---");
+            carService.Add(new Car { Id = 5, BrandId = 1, ColorId = 3, DailyPrice = 320, Description = "Ford Kuga Diesel", ModelYear = 2021 });
+            Console.WriteLine("\nAfter Addition:");
+            GetAllCars(carService);
+
+            Console.WriteLine("\n---Update---");
+            carService.Update(new Car { Id = 1, BrandId = 1, ColorId = 3, DailyPrice = 320, Description = "Ford Connect Diesel", ModelYear = 2017 });
+            Console.WriteLine("\nAfter Update:");
+            GetAllCars(carService);
+
+            Console.WriteLine("\n---Delete---");
+            carService.Delete(new Car { Id = 1, BrandId = 1, ColorId = 3, DailyPrice = 320, Description = "Ford Focus Diesel", ModelYear = 2017 });
+            Console.WriteLine("\nAfter Delete:");
+            GetAllCars(carService);
+        }
+
+        static void GetAllCars(ICarService carService)
+        {
+            foreach (var car in carService.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        private List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car
+                {
+                    Id = 1, BrandId = 1, ColorId = 1, DailyPrice = 150, Description = "Ford Focus Diesel",
+                    ModelYear = 2019
+                },
+                new Car
+                {
+                    Id = 2, BrandId = 2, ColorId = 1, DailyPrice = 90, Description = "Fiat Linea Diesel",
+                    ModelYear = 2013
+                },
+                new Car
+                {
+                    Id = 3, BrandId = 3, ColorId = 3, DailyPrice = 250, Description = "Volkswagen Golf Gasoline",
+                    ModelYear = 2020
+                },
+                new Car
+                {
+                    Id = 4, BrandId = 4, ColorId = 2, DailyPrice = 95, Description = "Reanult Symbol Diesel",
+                    ModelYear = 2012
+                },
+            };
+        }
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            var carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+
+        public void Delete(Car car)
+        {
+            var carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+    }
+}
