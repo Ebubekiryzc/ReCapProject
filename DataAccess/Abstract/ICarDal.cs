94d0107d3cdc057b6f44355b0eda95d3011a5260
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-08-01 15:33:20 +0300

    Edited according to EfEntityRepositoryBase.

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.DataAccess;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace DataAccess.Abstract
 {
     public interface ICarDal : IEntityRepository<Car>
     {
+        List<CarDetailDto> GetCarsWithDetail();
     }
 }
