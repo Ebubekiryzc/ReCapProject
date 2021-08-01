c95212e64c58080bb0a627d249a6a4c87cf87537
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-08-01 15:36:17 +0300

    All services have been tried.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Business.Abstract;
 using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 
@@ -10,31 +11,166 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ICarService carService = new CarManager(new InMemoryCarDal());
-            Console.WriteLine("Get All:");
+            Console.WriteLine("---------------------- Brand Operations ----------------------");
+
+            IBrandService brandService = new BrandManager(new EfBrandDal());
+            Brand brand = new Brand { Name = "Ford" };
+            AddBrand(brandService, brand);
+            GetAllBrands(brandService);
+            GetBrandById(brandService, 1);
+            brand = new Brand { Id = 1, Name = "Toyota" };
+            UpdateBrand(brandService, brand);
+            DeleteBrand(brandService, brand);
+            Console.WriteLine("Addition again:");
+            brand = new Brand { Name = "Mercedes" };
+            AddBrand(brandService, brand);
+
+            Console.WriteLine("---------------------- Color Operations ----------------------");
+
+            IColorService colorService = new ColorManager(new EfColorDal());
+            Color color = new Color { Name = "Yellow" };
+            AddColor(colorService, color);
+            GetAllColors(colorService);
+            GetColorById(colorService, 1);
+            color = new Color { Id = 1, Name = "Green" };
+            UpdateColor(colorService, color);
+            DeleteColor(colorService, color);
+            Console.WriteLine("Addition again:");
+            color = new Color { Name = "Red" };
+            AddColor(colorService, color);
+
+            Console.WriteLine("---------------------- Car Operations ----------------------");
+
+            ICarService carService = new CarManager(new EfCarDal());
+            Car car = new Car { BrandId = 2, ColorId = 2, Description = "Kuga Diesel", DailyPrice = 180, ModelYear = 1999 };
+            AddCar(carService, car);
             GetAllCars(carService);
+            GetCarById(carService, 1);
+            GetAllCarsWithDetails(carService);
+            car = new Car { Id = 1, BrandId = 2, ColorId = 2, Description = "Kuga Gasoline", DailyPrice = 180, ModelYear = 1999 };
+            UpdateCar(carService, car);
+            DeleteCar(carService, car);
+        }
 
-            Console.WriteLine("\nGet by Id:");
-            Console.WriteLine(carService.GetById(1).Description);
+        private static void GetAllCarsWithDetails(ICarService carService)
+        {
+            foreach (var carDetailDto in carService.GetCarsWithDetail())
+            {
+                Console.WriteLine("{0} {1} {2} {3}", carDetailDto.CarName, carDetailDto.BrandName, carDetailDto.ColorName,
+                    carDetailDto.DailyPrice);
+            }
+        }
 
-            Console.WriteLine("\n---Add---");
-            carService.Add(new Car { Id = 5, BrandId = 1, ColorId = 3, DailyPrice = 320, Description = "Ford Kuga Diesel", ModelYear = 2021 });
-            Console.WriteLine("\nAfter Addition:");
-            GetAllCars(carService);
-
-            Console.WriteLine("\n---Update---");
-            carService.Update(new Car { Id = 1, BrandId = 1, ColorId = 3, DailyPrice = 320, Description = "Ford Connect Diesel", ModelYear = 2017 });
-            Console.WriteLine("\nAfter Update:");
-            GetAllCars(carService);
-
+        private static void DeleteColor(IColorService colorService, Color color)
+        {
             Console.WriteLine("\n---Delete---");
-            carService.Delete(new Car { Id = 1, BrandId = 1, ColorId = 3, DailyPrice = 320, Description = "Ford Focus Diesel", ModelYear = 2017 });
-            Console.WriteLine("\nAfter Delete:");
+            colorService.Delete(color);
+            Console.Write("\nAfter Delete ");
+            GetAllColors(colorService);
+        }
+
+        private static void UpdateColor(IColorService colorService, Color color)
+        {
+            Console.WriteLine("\n---Update---");
+            colorService.Update(color);
+            Console.Write("\nAfter Update ");
+            GetAllColors(colorService);
+        }
+
+        private static void AddColor(IColorService colorService, Color color)
+        {
+            Console.WriteLine("\n---Add---");
+            colorService.Add(color);
+            Console.Write("\nAfter Addition ");
+            GetAllColors(colorService);
+        }
+
+        private static void GetColorById(IColorService colorService, int id)
+        {
+            Console.WriteLine("\nGet by Id:");
+            Console.WriteLine(colorService.GetById(id).Name);
+        }
+
+        private static void GetAllColors(IColorService colorService)
+        {
+            Console.WriteLine("All Colors:");
+            foreach (var color in colorService.GetAll())
+            {
+                Console.WriteLine(color.Name);
+            }
+        }
+
+        private static void DeleteBrand(IBrandService brandService, Brand brand)
+        {
+            Console.WriteLine("\n---Delete---");
+            brandService.Delete(brand);
+            Console.Write("\nAfter Delete ");
+            GetAllBrands(brandService);
+        }
+        private static void UpdateBrand(IBrandService brandService, Brand brand)
+        {
+            Console.WriteLine("\n---Update---");
+            brandService.Update(brand);
+            Console.Write("\nAfter Update ");
+            GetAllBrands(brandService);
+        }
+
+        private static void AddBrand(IBrandService brandService, Brand brand)
+        {
+            Console.WriteLine("\n---Add---");
+            brandService.Add(brand);
+            Console.Write("\nAfter Addition ");
+            GetAllBrands(brandService);
+        }
+
+        private static void GetBrandById(IBrandService brandService, int id)
+        {
+            Console.WriteLine("\nGet by Id:");
+            Console.WriteLine(brandService.GetById(id).Name);
+        }
+
+        private static void GetAllBrands(IBrandService brandService)
+        {
+            Console.WriteLine("All Brands:");
+            foreach (var brand in brandService.GetAll())
+            {
+                Console.WriteLine(brand.Name);
+            }
+        }
+
+        private static void DeleteCar(ICarService carService, Car car)
+        {
+            Console.WriteLine("\n---Delete---");
+            carService.Delete(car);
+            Console.Write("\nAfter Delete ");
             GetAllCars(carService);
         }
 
-        static void GetAllCars(ICarService carService)
+        private static void UpdateCar(ICarService carService, Car car)
         {
+            Console.WriteLine("\n---Update---");
+            carService.Update(car);
+            Console.Write("\nAfter Update");
+            GetAllCars(carService);
+        }
+
+        private static void AddCar(ICarService carService, Car car)
+        {
+            Console.WriteLine("\n---Add---");
+            carService.Add(car);
+            Console.Write("\nAfter Addition ");
+            GetAllCars(carService);
+        }
+
+        private static void GetCarById(ICarService carService, int id)
+        {
+            Console.WriteLine("\nGet by Id:");
+            Console.WriteLine(carService.GetById(id).Description);
+        }
+
+        private static void GetAllCars(ICarService carService)
+        {
+            Console.WriteLine("All Cars:");
             foreach (var car in carService.GetAll())
             {
                 Console.WriteLine(car.Description);
