5430cbc178552ac58096273367d1a5ac57783436
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-08-07 14:30:46 +0300

    Result system added to the project.

---
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Utilities.Results
+{
+    public class ErrorDataResult<T> : DataResult<T>
+    {
+        public ErrorDataResult(T data, bool success, string message) : base(data, false, message)
+        {
+        }
+
+        public ErrorDataResult(T data, bool success) : base(data, false)
+        {
+        }
+
+        public ErrorDataResult(bool success, string message) : base(default, false, message)
+        {
+        }
+
+        public ErrorDataResult(bool success) : base(default, false)
+        {
+        }
+    }
+}
---
+++ Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Utilities.Results
+{
+    public class SuccessDataResult<T> : DataResult<T>
+    {
+        public SuccessDataResult(T data, bool success, string message) : base(data, true, message)
+        {
+        }
+
+        public SuccessDataResult(T data, bool success) : base(data, true)
+        {
+        }
+
+        public SuccessDataResult(bool success, string message) : base(default, true, message)
+        {
+        }
+
+        public SuccessDataResult(bool success) : base(default, true)
+        {
+        }
+    }
+}
