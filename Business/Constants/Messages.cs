374a98f1677275834d251ec0e10f1bad5206219e
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-08-07 15:21:00 +0300

    Static messages created.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string AllBrandsListed = "All brands listed.";
+        public static string BrandListedById = "Brand listed by id.";
+        public static string BrandAdded = "Brand added successfully.";
+        public static string BrandUpdated = "Brand updated successfully.";
+        public static string BrandDeleted = "Brand deleted successfully.";
+
+        public static string AllCarsListed = "All cars listed.";
+        public static string CarsListedByBrandId = "Cars listed by brand id.";
+        public static string CarsListedByColorId = "Cars listed by color id.";
+        public static string CarListedById = "Car listed by id.";
+        public static string CarsListedWithDetail = "All cars listed with details.";
+        public static string CarAddingError = "An error has occurred when adding car.";
+        public static string CarAdded = "Car added successfully.";
+        public static string CarUpdated = "Car updated successfully.";
+        public static string CarDeleted = "Car deleted successfully.";
+
+        public static string AllColorsListed = "All colors listed.";
+        public static string ColorListById = "Color listed by id.";
+        public static string ColorAdded = "Color added successfully.";
+        public static string ColorUpdated = "Color updated successfully.";
+        public static string ColorDeleted = "Color deleted successfully.";
+    }
+}
