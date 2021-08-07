f8aa209ec15e6117df0c11c8ba0382741e6d41a6
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-08-07 19:13:01 +0300

    Edited some messages.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,33 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Business.Constants
+﻿namespace Business.Constants
 {
     public static class Messages
     {
-        public static string AllBrandsListed = "All brands listed.";
+        public static string OperationSuccessful = "Operation successful.";
+
+        public static string BrandsListed = "Brands listed successfully.";
         public static string BrandListedById = "Brand listed by id.";
-        public static string BrandAdded = "Brand added successfully.";
-        public static string BrandUpdated = "Brand updated successfully.";
-        public static string BrandDeleted = "Brand deleted successfully.";
 
-        public static string AllCarsListed = "All cars listed.";
-        public static string CarsListedByBrandId = "Cars listed by brand id.";
-        public static string CarsListedByColorId = "Cars listed by color id.";
+        public static string CarsListed = "Cars listed successfully.";
         public static string CarListedById = "Car listed by id.";
-        public static string CarsListedWithDetail = "All cars listed with details.";
-        public static string CarAddingError = "An error has occurred when adding car.";
-        public static string CarAdded = "Car added successfully.";
-        public static string CarUpdated = "Car updated successfully.";
-        public static string CarDeleted = "Car deleted successfully.";
+        public static string CarInvalid = "Car properties invalid for this operation.";
 
-        public static string AllColorsListed = "All colors listed.";
-        public static string ColorListById = "Color listed by id.";
-        public static string ColorAdded = "Color added successfully.";
-        public static string ColorUpdated = "Color updated successfully.";
-        public static string ColorDeleted = "Color deleted successfully.";
+        public static string ColorsListed = "Colors listed successfully.";
+        public static string ColorListedById = "Color listed by id.";
+
+        public static string CustomersListed = "Customers listed successfully.";
+        public static string CustomerListedById = "Customer listed by id.";
+
+        public static string RentalsListed = "Rentals listed successfully.";
+        public static string RentalListedById = "Rental listed by id.";
+
+        public static string UsersListed = "Users listed successfully.";
+        public static string UserListedById = "User listed by id.";
     }
 }
