a5c8132d5fcdb9b8c2d52b79656a5dd4f4f12ee0
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-08-07 14:38:25 +0300

    Constructor parameters fixed.

--- Core/Utilities/Results/ErrorDataResult.cs
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -2,19 +2,19 @@
 {
     public class ErrorDataResult<T> : DataResult<T>
     {
-        public ErrorDataResult(T data, bool success, string message) : base(data, false, message)
+        public ErrorDataResult(T data, string message) : base(data, false, message)
         {
         }
 
-        public ErrorDataResult(T data, bool success) : base(data, false)
+        public ErrorDataResult(T data) : base(data, false)
         {
         }
 
-        public ErrorDataResult(bool success, string message) : base(default, false, message)
+        public ErrorDataResult(string message) : base(default, false, message)
         {
         }
 
-        public ErrorDataResult(bool success) : base(default, false)
+        public ErrorDataResult() : base(default, false)
         {
         }
     }
