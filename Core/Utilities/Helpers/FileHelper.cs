d8e003d2fe8b44a88357b77385e33d9bf0b4c032
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-10 00:04:18 +0300

    FileHelper created for image uploads to the system.

---
+++ Core/Utilities/Helpers/FileHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using Core.Utilities.Results;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Utilities.Helpers
+{
+    public static class FileHelper
+    {
+        public static IResult AddAsync(IFormFile file, string path)
+        {
+            string fileNameWithGUID;
+            try
+            {
+                if (file == null) throw new FileNotFoundException();
+
+                fileNameWithGUID = $"{Guid.NewGuid().ToString()}{Path.GetExtension(file.FileName)}";
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                using (FileStream fileStream = new FileStream(Path.Combine(path, fileNameWithGUID), FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                    fileStream.Flush();
+                }
+            }
+            catch (Exception e)
+            {
+                return new ErrorResult(e.Message);
+            }
+
+            return new SuccessResult(Path.Combine(path, fileNameWithGUID));
+        }
+
+        public static IResult DeleteAsync(string path)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    throw new FileNotFoundException();
+                }
+                File.Delete(path);
+            }
+            catch (Exception e)
+            {
+                return new ErrorResult(e.Message);
+            }
+
+            return new SuccessResult("Operation Successful!");
+        }
+
+    }
+}
