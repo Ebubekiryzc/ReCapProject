3d4398f9c47fa20ea4f9892c1330b0148813a8b7
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-12 22:47:32 +0300

    Added extensions for Claim.

---
+++ Core/Extensions/ClaimExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.IdentityModel.JsonWebTokens;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Core.Extensions
+{
+    public static class ClaimExtensions
+    {
+        public static void AddEmail(this ICollection<Claim> claims, string email)
+        {
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, email));
+        }
+
+        public static void AddNameIdentifier(this ICollection<Claim> claims, string nameIdentifier)
+        {
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, nameIdentifier));
+        }
+
+        public static void AddRoles(this ICollection<Claim> claims, string[] roles)
+        {
+            roles.ToList().ForEach(role => claims.Add(new Claim(ClaimTypes.Role, role)));
+        }
+    }
+}
