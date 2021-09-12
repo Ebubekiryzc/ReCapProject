0fe41e6207d7474ff335c0034adf66f1276d16b9
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-12 15:04:51 +0300

    Entity modified according to changes in database tables.

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -5,9 +5,9 @@ namespace Entities.Concrete
     public class User : IEntity
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public bool Status { get; set; }
     }
 }
