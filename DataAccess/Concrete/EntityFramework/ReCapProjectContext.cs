6079461df92c57a7edfd296721a18c14929067af
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-12 15:07:30 +0300

    Edited according to newly added entities and changes in database tables.

--- DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
+++ DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
@@ -13,13 +13,15 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Car> Cars { get; set; }
         public DbSet<CarImage> CarImages { get; set; }
         public DbSet<Color> Colors { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<IndividualCustomer> IndividualCustomers { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
         public DbSet<Rental> Rentals { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Customer>().Property(c => c.Id).HasColumnName("UserId");
+            modelBuilder.Entity<IndividualCustomer>().Property(c => c.Id).HasColumnName("UserId");
         }
     }
 }
