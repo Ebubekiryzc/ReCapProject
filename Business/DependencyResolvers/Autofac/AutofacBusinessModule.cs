c2d1ec3fde6b92622accf02f41388784cf035967
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-12 23:44:07 +0300

    IoC adjusted for newly added entities.

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -4,8 +4,10 @@ using Business.Abstract;
 using Business.Concrete;
 using Castle.DynamicProxy;
 using Core.Utilities.Interceptors;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+//using Microsoft.AspNetCore.Http;
 
 namespace Business.DependencyResolvers.Autofac
 {
@@ -13,27 +15,33 @@ namespace Business.DependencyResolvers.Autofac
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.RegisterType<AuthManager>().As<IAuthService>().SingleInstance();
+            //<--------------------------------------------------------------------------------------------------->                              
             builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
             builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
-
+            //<--------------------------------------------------------------------------------------------------->                              
             builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
             builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
-
+            //<--------------------------------------------------------------------------------------------------->                              
             builder.RegisterType<CarImageManager>().As<ICarImageService>().SingleInstance();
             builder.RegisterType<EfCarImageDal>().As<ICarImageDal>().SingleInstance();
-
+            //<--------------------------------------------------------------------------------------------------->                              
             builder.RegisterType<ColorManager>().As<IColorService>().SingleInstance();
             builder.RegisterType<EfColorDal>().As<IColorDal>().SingleInstance();
-
+            //<--------------------------------------------------------------------------------------------------->                              
             builder.RegisterType<IndividualCustomerManager>().As<IIndividualCustomerService>().SingleInstance();
             builder.RegisterType<EfIndividualCustomerDal>().As<IIndividualCustomerDal>().SingleInstance();
-
+            //<--------------------------------------------------------------------------------------------------->                              
+            //builder.RegisterType<HttpContextAccessor>().As<IHttpContextAccessor>().SingleInstance();
+            //<--------------------------------------------------------------------------------------------------->                              
             builder.RegisterType<RentalManager>().As<IRentalService>().SingleInstance();
             builder.RegisterType<EfRentalDal>().As<IRentalDal>().SingleInstance();
-
+            //<--------------------------------------------------------------------------------------------------->                              
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>().SingleInstance();
+            //<--------------------------------------------------------------------------------------------------->                              
             builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
             builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
-
+            //<--------------------------------------------------------------------------------------------------->                              
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
             builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
