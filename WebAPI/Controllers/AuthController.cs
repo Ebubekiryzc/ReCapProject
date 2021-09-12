3797137e0b7a566637fd16aad7c9844046336050
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-12 23:43:25 +0300

    Auth system added to the project.

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(UserForLoginDto userForLoginDto)
+        {
+            var userToLogin = _authService.Login(userForLoginDto);
+            if (!userToLogin.Success)
+            {
+                return BadRequest(userToLogin.Message);
+            }
+
+            var result = _authService.CreateAccessToken(userToLogin.Data);
+            return ReturnResult(result);
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserForRegisterDto userForRegisterDto)
+        {
+            var userExists = _authService.UserExists(userForRegisterDto.Email);
+            if (!userExists.Success)
+            {
+                return BadRequest(userExists.Message);
+            }
+
+            var registerResult = _authService.Register(userForRegisterDto);
+            var result = _authService.CreateAccessToken(registerResult.Data);
+            return ReturnResult(result);
+        }
+
+        private IActionResult ReturnResult(IResult result)
+        {
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
