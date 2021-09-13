de3b753aa309992e6fcc4be3dcf70d9b46436061
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-13 21:09:33 +0300

    Core dependencies loaded for service.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,3 +1,6 @@
+using Core.DependencyResolvers;
+using Core.Extensions;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -72,6 +75,8 @@ namespace WebAPI
                     IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                 };
             });
+
+            services.AddDependencyResolvers(new ICoreModule[] { new CoreModule() });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
