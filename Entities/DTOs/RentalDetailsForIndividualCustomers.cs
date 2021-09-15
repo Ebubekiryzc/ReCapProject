f7d0acf08b1792ebe77f64b7229cc508ebc90554
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-15 23:50:00 +0300

    Rental details created for individual customers.

---
+++ Entities/DTOs/RentalDetailsForIndividualCustomers.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Abstract;
+using System;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailsForIndividualCustomers : IDto
+    {
+        public int Id { get; set; }
+        public string BrandName { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
