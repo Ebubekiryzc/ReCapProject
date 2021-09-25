9c718ea3b05964ebe4af1f3a5ae27bbf2eb5d8a7
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-25 17:57:00 +0300

    Modified according to http-server package.

--- Business/Constants/DefaultRoutes.cs
+++ Business/Constants/DefaultRoutes.cs
@@ -4,7 +4,7 @@ namespace Business.Constants
 {
     public static class DefaultRoutes
     {
-        public static string DefaultImageFolder = $"{Directory.GetParent(Directory.GetCurrentDirectory())}\\WebAPI\\wwwroot\\images\\";
+        public static string DefaultImageFolder = @"http://127.0.0.1:8080\WebAPI\wwwroot\images\";
         public static string DefaultImage = $"{DefaultImageFolder}defaultLogo.jpg";
     }
 }
