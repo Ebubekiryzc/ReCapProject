f992965fa5087ec2e4e498ec9c836c830dad03f8
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-25 17:58:55 +0300

    Modified according to new methods.

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -9,7 +9,7 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car, ReCapProjectContext>, ICarDal
     {
-        public List<CarDetailDto> GetCarsWithDetail()
+        public List<CarDetailDto> GetCarsWithDetails()
         {
             using (ReCapProjectContext context = new ReCapProjectContext())
             {
@@ -18,6 +18,86 @@ namespace DataAccess.Concrete.EntityFramework
                              join color in context.Colors on car.ColorId equals color.Id
                              select new CarDetailDto
                              {
+                                 CarId = car.Id,
+                                 CarName = car.Description,
+                                 BrandName = brand.Name,
+                                 ColorName = color.Name,
+                                 DailyPrice = car.DailyPrice
+                             };
+                return result.ToList();
+            }
+        }
+
+        public CarDetailDto GetCarWithDetailsById(int id)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                var result = from car in context.Cars
+                             join brand in context.Brands on car.BrandId equals brand.Id
+                             join color in context.Colors on car.ColorId equals color.Id
+                             where car.Id == id
+                             select new CarDetailDto
+                             {
+                                 CarId = car.Id,
+                                 CarName = car.Description,
+                                 BrandName = brand.Name,
+                                 ColorName = color.Name,
+                                 DailyPrice = car.DailyPrice
+                             };
+                return result.SingleOrDefault();
+            }
+        }
+
+        public List<CarDetailDto> GetCarsWithDetailsByBrandId(int brandId)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                var result = from car in context.Cars
+                             join brand in context.Brands on car.BrandId equals brand.Id
+                             join color in context.Colors on car.ColorId equals color.Id
+                             where car.BrandId == brandId
+                             select new CarDetailDto
+                             {
+                                 CarId = car.Id,
+                                 CarName = car.Description,
+                                 BrandName = brand.Name,
+                                 ColorName = color.Name,
+                                 DailyPrice = car.DailyPrice
+                             };
+                return result.ToList();
+            }
+        }
+
+        public List<CarDetailDto> GetCarsWithDetailsByColorId(int colorId)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                var result = from car in context.Cars
+                             join brand in context.Brands on car.BrandId equals brand.Id
+                             join color in context.Colors on car.ColorId equals color.Id
+                             where car.ColorId == colorId
+                             select new CarDetailDto
+                             {
+                                 CarId = car.Id,
+                                 CarName = car.Description,
+                                 BrandName = brand.Name,
+                                 ColorName = color.Name,
+                                 DailyPrice = car.DailyPrice
+                             };
+                return result.ToList();
+            }
+        }
+        public List<CarDetailDto> GetCarsWithDetailsByBrandIdAndColorId(int brandId, int colorId)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                var result = from car in context.Cars
+                             join brand in context.Brands on car.BrandId equals brand.Id
+                             join color in context.Colors on car.ColorId equals color.Id
+                             where car.BrandId == brandId && car.ColorId == colorId
+                             select new CarDetailDto
+                             {
+                                 CarId = car.Id,
                                  CarName = car.Description,
                                  BrandName = brand.Name,
                                  ColorName = color.Name,
