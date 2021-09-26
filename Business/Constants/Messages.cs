7eb24f9ae2bec2a6eb7f71c385602d07043ee4e0
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-26 19:24:39 +0300

    Added new constant messages for new entities.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,39 +1,52 @@
-﻿using Core.Entities.Concrete;
-
-namespace Business.Constants
+﻿namespace Business.Constants
 {
     public static class Messages
     {
         public static string OperationSuccessful = "Operation successful.";
-        public static string OperationFailed = "Operation failed.";
+        public static string OperationFailed = "Operation failed!";
 
         public static string BrandsListed = "Brands listed successfully.";
         public static string BrandListed = "Brand listed successfully.";
 
         public static string CarsListed = "Cars listed successfully.";
         public static string CarListed = "Car listed successfully.";
-        public static string CarInvalid = "Car properties invalid for this operation.";
+        public static string CarInvalid = "Car properties invalid for this operation!";
 
-        public static string MaxImageOfCarExceeded = "You can add maximum of 5 images for each car.";
+        public static string MaxImageOfCarExceeded = "You can add maximum of 5 images for each car!";
 
         public static string ColorsListed = "Colors listed successfully.";
         public static string ColorListed = "Color listed successfully.";
 
+        public static string CreditCardsListed = "Credit cards listed successfully.";
+        public static string CreditCardListed = "Credit card listed successfully.";
+
+        public static string CreditCardTypesListed = "Credit card types listed successfully.";
+        public static string CreditCardTypeListed = "Credit card type listed successfully.";
+        public static string CreditCardNotFound = "Credit card not found!";
+        public static string CreditCardNumberNotMatching = "Card number does not match!";
+        public static string CvcNotMatching = "CVC number does not match";
+        public static string ExpirationDateNotMatching = "Expiration date does not match!";
+
+        public static string UserCreditCardsListed = "Credit cards of user listed successfully.";
+        public static string UserCreditCardListed = "Credit card of user listed successfully.";
+
         public static string CustomersListed = "Customers listed successfully.";
         public static string CustomerListed = "IndividualCustomer listed successfully.";
 
+        public static string PaymentSuccessful = "Payment successfully completed.";
+
         public static string RentalsListed = "Rentals listed successfully.";
         public static string RentalListed = "Rental listed successfully.";
-        public static string DateInvalid = "The date ranges entered are incorrect.";
+        public static string DateInvalid = "The date ranges entered are incorrect!";
 
         public static string UsersListed = "Users listed successfully.";
         public static string UserListed = "User listed successfully.";
         public static string UserClaimsListed = "User claims listed successfully.";
         public static string UserNotFound = "User not found!";
-        public static string PasswordError = "Password does not match.";
-        public static string UserAlreadyExists = "User already exists.";
+        public static string PasswordError = "Password does not match!";
+        public static string UserAlreadyExists = "User already exists!";
         public static string RegistrationSuccessful = "Registration completed successfully.";
         public static string AccessTokenCreatedSuccessfully = "Access token created.";
-        public static string AuthorizationDenied = "You are not allowed for that action.";
+        public static string AuthorizationDenied = "You are not allowed for that action!";
     }
 }
