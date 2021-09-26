178b1cb96c4f2c79de96d95cb7fb82237bd501ba
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-26 19:26:18 +0300

    DTO' s created for newly added entities.

---
+++ Entities/DTOs/CreditCardForUserOperationsDto.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Abstract;
+using System;
+
+namespace Entities.DTOs
+{
+    public class CreditCardForUserOperationsDto : IDto
+    {
+        public int Id { get; set; }
+        public int CreditCardTypeId { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string Cvc { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
---
+++ Entities/DTOs/PaymentQueryDto.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Abstract;
+using Entities.Concrete;
+
+namespace Entities.DTOs
+{
+    public class PaymentQueryDto : IDto
+    {
+        public UserCreditCard UserCreditCard { get; set; }
+        public Rental Rental { get; set; }
+    }
+}
